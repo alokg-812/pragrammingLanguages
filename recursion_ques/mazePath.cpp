71ef5274e0ae37a087303efd6d2d622e830f96d0
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-04 13:25:32 +0530

    code

--- recursion_ques/mazePath.cpp
+++ recursion_ques/mazePath.cpp
@@ -10,9 +10,20 @@ int maze(int sr, int sc, int er, int ec){
     return totalWays;
 }
 
+void printPath(int sr, int sc, int er, int ec, string s){ // string will store the path of the maze
+    if(sr>er || sc>ec) return ;
+    if((sc==ec) && (sr==er)){ //destination reached
+        cout<<s<<endl;
+        return;
+    }
+    printPath(sr, sc+1, er, ec, s + 'R'); // Rightways
+    printPath(sr+1, sc, er, ec, s + 'D'); // Downways
+}
+
 int main(){
     int r,c;
     cout<<"Enter rows and cols: ";
     cin>>r>>c;
-    cout<<maze(0,0,1,2);
+    cout<<maze(1,1,r,c)<<endl;
+    printPath(1,1,r,c, "");
 }
