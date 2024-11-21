123a95ed42af8ec66ee4b5b91295e1036b405975
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:10:34 +0530

    Create fiveSubjectPercentage.java

---
+++ Java/fiveSubjectPercentage.java
@@ -0,0 +1,12 @@
+public class PerceOf5 {
+    public static void main(String[] args) {
+        //code for % of 5 subjects
+        double x1 = 100; //maths
+        double x2 = 91; //physics
+        double x3 = 87; //chem
+        double x4 = 77; //english
+        double x5 = 34; // physical
+        double percent = (x1+x2+x3+x4+x5)/ 5;
+        System.out.println(percent);
+    }
+}
