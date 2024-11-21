27c7b8892ea6b4ffd85d28e05e399387e79a7cd2
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:12:26 +0530

    Create basics.java

---
+++ Java/basics.java
@@ -0,0 +1,8 @@
+class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Alok G");
+        System.out.print("Hello World!1");
+        System.out.print("Alok G1");
+    }
+}
