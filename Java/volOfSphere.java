8b22fb1172e25943a19f58a08776726bda494221
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:15:26 +0530

    Create volOfSphere.java

---
+++ Java/volOfSphere.java
@@ -0,0 +1,8 @@
+public class volOfSphere {
+    public static void main(String[] args) {
+        double rad = 8;
+        double pi = 3.1415;
+        double vol = (4*pi*rad*rad*rad)/3;
+        System.out.println(vol);
+    }
+}
