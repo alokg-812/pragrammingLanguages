e2ed77cabbd43fbd15d3ec2da39e6ca76a0a2b8f
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:14:31 +0530

    Create sumOf2.java

---
+++ Java/sumOf2.java
@@ -0,0 +1,8 @@
+public class sumOf2 {
+    public static void main(String[] args) {
+        int x = 7, y = 10;
+        int z = x+y;
+        System.out.println(x+y);
+        System.out.println(z);
+    }
+}
