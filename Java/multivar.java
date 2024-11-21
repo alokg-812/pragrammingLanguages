d3560067b29fa09fc753c51111e5e1ed8045bff1
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:13:39 +0530

    Create multivar.java

---
+++ Java/multivar.java
@@ -0,0 +1,9 @@
+public class multivar {
+    public static void main(String[] args) {
+        int x = 5 , y = 8 , z = 10;
+        System.out.println("Hello"+x+y);
+        System.out.println("Hello"+x+y+z);
+        //System.out.println(+x ,"Hello" +y+z);
+       // System.out.println(+x+y""+z);
+    }
+}
