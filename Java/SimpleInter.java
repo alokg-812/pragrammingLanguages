3381c098d5027168ea5c2c7aeb526ac0a4357a7e
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:11:31 +0530

    Create SimpleInter.java

---
+++ Java/SimpleInter.java
@@ -0,0 +1,7 @@
+public class SimpleInter {
+    public static void main(String[] args) {
+        double principle = 10000, rate = 12 , time = 5;
+        double simple_interet = (principle*rate*time) / 100;
+        System.out.println(simple_interet);
+    }
+}
