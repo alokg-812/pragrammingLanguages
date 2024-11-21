3c3fbaae167b5075a981cd7d4e92a516f197f1ac
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:13:16 +0530

    Create inreaDecre.java

---
+++ Java/inreaDecre.java
@@ -0,0 +1,11 @@
+public class inreaDecre {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x);
+
+    }
+}
