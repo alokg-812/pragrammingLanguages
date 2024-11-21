363ed29029df24c9143bab5642b78612678c2b75
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:07:42 +0530

    Create areaOfCircle.java

---
+++ Java/areaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOFCircle {
+    public static void main(String[] args) {
+        double rad = 10;
+        double pi = 3.141592653589793238;
+        double area = pi*rad*rad;
+        double circum = 2*pi*rad;
+        System.out.println("circum "+circum);
+        System.out.println("area: "+area);
+    }
+}
