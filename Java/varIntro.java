ef0e26f20be8f17f05e201ef2c9c89db6472f785
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-21 16:14:56 +0530

    Create varIntro.java

---
+++ Java/varIntro.java
@@ -0,0 +1,10 @@
+public class varIntro {
+    public static void main(String[] args) {
+        int a,b;
+        a = 5;
+        b = 456;
+        System.out.println(a);
+        System.out.println(b);
+    
+    }
+}
